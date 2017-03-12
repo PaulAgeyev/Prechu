da2aa36878e4fc9003d50f0987576e797af173ee
Pavel <pavel@Pavels-MacBook-Pro.local>
2017-03-12 15:03:52 +0200

    no message

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+    });
+    
+});
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}  
